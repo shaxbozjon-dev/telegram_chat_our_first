423df82a3fcf64a35922cd6e1aa238f4a231f690
shaxbozjon-dev <shaxbozjon215@gmail.com>
2023-06-16 21:06:58 +0500

    uxshadimi

---
+++ our_project_teleram_chat/bin/our_project_teleram_chat.dart
@@ -0,0 +1,5 @@
+import 'package:our_project_teleram_chat/our_project_teleram_chat.dart' as our_project_teleram_chat;
+
+void main(List<String> arguments) {
+  print('Hello world: ${our_project_teleram_chat.calculate()}!');
+}
---
+++ our_project_teleram_chat/test/our_project_teleram_chat_test.dart
@@ -0,0 +1,8 @@
+import 'package:our_project_teleram_chat/our_project_teleram_chat.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
