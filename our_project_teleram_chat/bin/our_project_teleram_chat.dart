9f81b342013e0650f9a3915c9de70cd16b7e5b97
erkinoff <murodjonerkinov2005@gmail.com>
2023-06-16 21:16:21 +0500

    .

--- our_project_teleram_chat/bin/our_project_teleram_chat.dart
+++
@@ -1,5 +0,0 @@
-import 'package:our_project_teleram_chat/our_project_teleram_chat.dart' as our_project_teleram_chat;
-
-void main(List<String> arguments) {
-  print('Hello world: ${our_project_teleram_chat.calculate()}!');
-}
