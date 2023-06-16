e35044bc2154cb829dbd17b921bf332259931a55
shaxbozjon-dev <shaxbozjon215@gmail.com>
2023-06-16 21:44:52 +0500

    hello

--- our_project_teleram_chat/bin/our_project_teleram_chat.dart
+++ our_project_teleram_chat/bin/our_project_teleram_chat.dart
@@ -1,5 +1,1 @@
-import 'package:our_project_teleram_chat/our_project_teleram_chat.dart' as our_project_teleram_chat;
-
-void main(List<String> arguments) {
-  print('Hello world: ${our_project_teleram_chat.calculate()}!');
-}
+void main(List<String> arguments) {}
