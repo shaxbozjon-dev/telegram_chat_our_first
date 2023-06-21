8bd935465b0bf3b98d96a18ff054732a6505f25c
erkinoff <murodjonerkinov2005@gmail.com>
2023-06-22 01:07:16 +0500

    tekshirib ko'rin

--- our_project_teleram_chat/bin/main.dart
+++ our_project_teleram_chat/bin/main.dart
@@ -1,3 +1,3 @@
-import 'package:our_project_teleram_chat/pages/view.dart';
+// import 'package:our_project_teleram_chat/pages/view.dart';
 
-void main(List<String> args) => menu.builder();
+// void main(List<String> args) => menu.builder();
--- our_project_teleram_chat/lib/pages/view.dart
+++ our_project_teleram_chat/lib/pages/view.dart
@@ -1,60 +1,60 @@
-import 'package:our_project_teleram_chat/data_base/chat_database.dart';
-import 'package:our_project_teleram_chat/models/chat.dart';
+// import 'package:our_project_teleram_chat/data_base/chat_database.dart';
+// import 'package:our_project_teleram_chat/models/chat.dart';
 
-import '../data_base/data_base.dart';
-import '../models/../models/user_model.dart';
-// import 'package:uuid/uuid.dart';
-import '../pages/builder.dart';
-import '../service/io_service.dart';
+// import '../data_base/data_base.dart';
+// import '../models/../models/user_model.dart';
+// // import 'package:uuid/uuid.dart';
+// import '../pages/builder.dart';
+// import '../service/io_service.dart';
 
-Menu menu = Menu();
+// Menu menu = Menu();
 
-class Menu extends Builder {
-  @override
-  void builder() {
-    // TODO: implement builder
-    super.builder();
+// class Menu extends Builder {
+//   @override
+//   void builder() {
+//     // TODO: implement builder
+//     super.builder();
 
-    while (true) {
-      String common = io.outputText(
-          "_________________Xush kelibsiz___________________\n1.SignIn\n2.SignUp\n3.exit\n");
-      switch (common) {
-        case "1":
-          signIn();
-          break;
-        case "2":
-          signUp();
-          break;
-        default:
-      }
-    }
-  }
-}
+//     while (true) {
+//       String common = io.outputText(
+//           "_________________Xush kelibsiz___________________\n1.SignIn\n2.SignUp\n3.exit\n");
+//       switch (common) {
+//         case "1":
+//           signIn();
+//           break;
+//         case "2":
+//           signUp();
+//           break;
+//         default:
+//       }
+//     }
+//   }
+// }
 
-void signUp() {
-  String number = "";
-  String name = "";
-  String password = "";
-  db.takror(number, name, password);
-}
+// void signUp() {
+//   String number = "";
+//   String name = "";
+//   String password = "";
+//   db.takror(number, name, password);
+// }
 
-void signIn() {
-  String number = io.outputText("******Sign UP*****\n number kiriting: ");
-  String password = io.outputText(" parol kiriting: ");
-  print(db.readUser(number));
-  db.userData[number];
-  String see = io.outputText("1.read contakt\n2.chat\n ");
-  switch (see) {
-    case "1":
-      print(db.readAllUsers());
-      break;
-    case "2":
-      String qabulQiluvchi =
-          io.outputText("qabul qiluvchi odemni raqamini kiriting 🔛 ");
-      String message = io.outputText("junatuvchi odemni raqamini kiriting 🔛 ");
-      Chat chat = Chat(sender: qabulQiluvchi, receiver: message);
-      ChatBase().addMessage("", qabulQiluvchi, message);
+// void signIn() {
+//   String number = io.outputText("******Sign UP*****\n number kiriting: ");
+//   String password = io.outputText(" parol kiriting: ");
+//   print(db.readUser(number));
+//   db.userData[number];
+//   String see = io.outputText("1.read contakt\n2.chat\n ");
+//   switch (see) {
+//     case "1":
+//       print(db.readAllUsers());
+//       break;
+//     case "2":
+//       String qabulQiluvchi =
+//           io.outputText("qabul qiluvchi odemni raqamini kiriting 🔛 ");
+//       String message = io.outputText("junatuvchi odemni raqamini kiriting 🔛 ");
+//       Chat chat = Chat(sender: qabulQiluvchi, receiver: message);
+//       ChatBase().addMessage("", qabulQiluvchi, message);
 
-    default:
-  }
-}
+//     default:
+//   }
+// }
