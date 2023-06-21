e3b2b65d25e27238a0475363b72a56b8b61347e6
shaxbozjon-dev <shaxbozjon215@gmail.com>
2023-06-22 01:01:41 +0500

    1

--- our_project_teleram_chat/lib/pages/view.dart
+++ our_project_teleram_chat/lib/pages/view.dart
@@ -19,7 +19,7 @@ class Menu extends Builder {
           "_________________Xush kelibsiz___________________\n1.SignIn\n2.SignUp\n3.exit\n");
       switch (common) {
         case "1":
-          SignIn();
+          signIn();
           break;
         case "2":
           signUp();
@@ -34,11 +34,10 @@ void signUp() {
   String number = "";
   String name = "";
   String password = "";
-  bool check = false;
   db.takror(number, name, password);
 }
 
-void SignIn() {
+void signIn() {
   String number = io.outputText("******Sign UP*****\n number kiriting: ");
   String password = io.outputText(" parol kiriting: ");
   print(db.readUser(number));
